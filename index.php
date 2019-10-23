b147cff4a1b9928f21d2d744b246804171bdfe82
arturo <artedsolis@gmail.com>
2019-10-23 15:00:54 +0200

    inclusion d'une partie du code request sur inde_post

--- index.php
+++ index.php
@@ -54,19 +54,6 @@ catch (Exception $e)
   die('Erreur : ' . $e->getMessage());
 }
 
-  if(isset ($_POST['username'], $_POST['email'], $_POST['password']))
-  {
-  $req = $bdd->prepare('INSERT INTO student(username,email,password) VALUES(:username, :email, :password)');
-  $req->execute(array(
-  'username' => $username,
-  'email' => $email,
-  'password' => $password  
-  ));  
-  }
-
-  
-  ?>
-
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
